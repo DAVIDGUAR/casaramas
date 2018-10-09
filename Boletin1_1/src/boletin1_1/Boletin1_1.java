5b9d23871d74ed5360b806f925a17ea672e64e0a
palonsovazquez <>
2018-10-09 10:45:49 +0200

    resuelto boletin 1_2

--- Boletin1_1/src/boletin1_1/Boletin1_1.java
+++ Boletin1_1/src/boletin1_1/Boletin1_1.java
@@ -5,17 +5,71 @@
  */
 package boletin1_1;
 
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 /**
  *
- * @author pablo
+ * @author palonsovazquez
  */
 public class Boletin1_1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static Scanner sca = new Scanner(System.in);
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic herefsafsdfsdfsafsadfsafsdasafdsaf
+        triangulo();
+        
+        
+        
+        
     }
+    
+    public static void triangulo(){
+    Double base,area,altura;
+    
+    
+    base = pedirNumero("base"); 
+    altura = pedirNumero("altura");
+    area = (base*altura)/2;
+    System.out.println("El area del triangulo de base: " +base+ " y altura: "+altura+" es: "+ area);
+    
+    
+    }
+    
+    public static Double pedirNumero(String nombreDato){
+    Double res;
+        do {
+    System.out.println("Introduzca la "+ nombreDato+":");
+    res = comprobarNum( sca.nextLine());
+    }while(res == null);  // pedira el dato hasta que e introduzca un numero valido.
+    return res;
+    
+    
+    }
+     public static  Double comprobarNum(String numero){
+         Double x ;
+        try{   // lo parseo y si no es un numero valido asigna a x el valor de nulo.
+           x =  Double.parseDouble(numero); 
+        }catch(Exception ex){
+            x=null;
+            
+     }
+      if (x <= 0)         //compruebo si es un numero valido, es decir, si es positivo y distinto de 0.
+      {
+      x = null;
+      }      
+            
+            
+            return x;
+      
+        
+        
+
+
+
+}    
+    
     
 }
--- Boletin1_2/src/boletin1_2/Boletin1_2.java
+++ Boletin1_2/src/boletin1_2/Boletin1_2.java
@@ -5,17 +5,68 @@
  */
 package boletin1_2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pablo
  */
 public class Boletin1_2 {
 
-    /**
-     * @param args the command line arguments
-     */
+     public static Scanner sca = new Scanner(System.in);
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here sdfsdafdsafasdfasfasfdswevvcxa
+        cuadrado();
+        
+        
+        
+        
     }
+    
+    public static void cuadrado(){
+    Double base,area,altura;
+    
+    
+    base = pedirNumero("base"); 
+    altura = pedirNumero("altura");
+    area = (base*altura);
+    System.out.println("El area del cuadrado de base: " +base+ " y altura: "+altura+" es: "+ area);
+    
+    
+    }
+    
+    public static Double pedirNumero(String nombreDato){
+    Double res;
+        do {
+    System.out.println("Introduzca la "+ nombreDato+":");
+    res = comprobarNum( sca.nextLine());
+    }while(res == null);  // pedira el dato hasta que e introduzca un numero valido.
+    return res;
+    
+    
+    }
+     public static  Double comprobarNum(String numero){
+         Double x ;
+        try{   // lo parseo y si no es un numero valido asigna a x el valor de nulo.
+           x =  Double.parseDouble(numero); 
+        }catch(Exception ex){
+            x=null;
+            
+     }
+      if (x <= 0)         //compruebo si es un numero valido, es decir, si es positivo y distinto de 0.
+      {
+      x = null;
+      }      
+            
+            
+            return x;
+      
+        
+        
+
+
+
+}    
     
 }
